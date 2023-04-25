eb2460bd99d00a31b9407a082f3d4307ed79aa84
Gregory M. Antunes Pires <68088354+gregoryantunes@users.noreply.github.com>
2023-04-25 19:32:25 -0300

    Update delete_random.py

--- delete_random.py
+++ delete_random.py
@@ -5,10 +5,10 @@ myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["mydatabase"]
 mycol = mydb["customers"]
 
-num_deletes = random.randint(30, 10000) # Randomly chooses the number of deletes to be performed
+num_deletes = random.randint(30, 10000)
 
 for i in range(num_deletes):
-    query = {"_id": random.choice(list(mycol.find()))["_id"]} # Randomly selects a document to delete
-    mycol.delete_one(query) # Deletes the document from the collection
+    query = {"_id": random.choice(list(mycol.find()))["_id"]}
+    mycol.delete_one(query) 
 
 print(f"{num_deletes} documents were deleted from the collection.")
