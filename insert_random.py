5669c6c289003aea9dbdd5c8049711d1431d4915
Gregory M. Antunes Pires <68088354+gregoryantunes@users.noreply.github.com>
2023-04-25 19:14:37 -0300

    Update insert_random.py

--- insert_random.py
+++ insert_random.py
@@ -6,12 +6,12 @@ myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["mydatabase"]
 mycol = mydb["customers"]
 
-num_inserts = random.randint(30, 10000) # Escolhe aleatoriamente o número de inserções a serem realizadas
+num_inserts = random.randint(30, 10000) # Randomly chooses the number of inserts to be performed
 
 for i in range(num_inserts):
-    name = ''.join(random.choice(string.ascii_letters) for i in range(10)) # Gera um nome aleatório de 10 letras
-    address = ''.join(random.choice(string.ascii_letters + string.digits) for i in range(20)) # Gera um endereço aleatório de 20 caracteres alfanuméricos
-    mycol.insert_one({"name": name, "address": address}) # Insere um documento na coleção com o nome e endereço aleatórios
+    name = ''.join(random.choice(string.ascii_letters) for i in range(10)) # Generates a random name with 10 letters
+    address = ''.join(random.choice(string.ascii_letters + string.digits) for i in range(20)) # Generates a random address with 20 alphanumeric characters
+    mydict = {"name": name, "address": address} # Creates a dictionary with the name and address
+    x = mycol.insert_one(mydict) # Inserts the document into the collection
 
-print(f"{num_inserts} documentos foram inseridos na coleção.")
-
+print(f"{num_inserts} documents were inserted into the collection.")
