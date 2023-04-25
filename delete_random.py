aa0b76bddb29203713155e1d85784626d77a49a3
Gregory M. Antunes Pires <68088354+gregoryantunes@users.noreply.github.com>
2023-04-25 19:13:42 -0300

    Update delete_random.py

--- delete_random.py
+++ delete_random.py
@@ -1,18 +1,17 @@
 import pymongo
 import random
+import string
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["mydatabase"]
 mycol = mydb["customers"]
 
-count = mycol.count_documents({}) # Obtém o número de documentos na coleção
-num_to_delete = random.randint(30, 10000) # Escolhe aleatoriamente o número de documentos a serem excluídos
-if num_to_delete >= count:
-    print("Não há documentos suficientes na coleção para excluir.")
-else:
-    random_docs = random.sample(range(0, count), num_to_delete) # Escolhe aleatoriamente os índices dos documentos a serem excluídos
-    docs_to_delete = [mycol.find()[i] for i in random_docs] # Obtém os documentos correspondentes aos índices escolhidos
-    for doc in docs_to_delete:
-        mycol.delete_one(doc) # Exclui cada documento
-    print(f"{num_to_delete} documentos foram excluídos da coleção.")
+num_inserts = random.randint(30, 10000) # Randomly chooses the number of inserts to be performed
 
+for i in range(num_inserts):
+    name = ''.join(random.choice(string.ascii_letters) for i in range(10)) # Generates a random name with 10 letters
+    address = ''.join(random.choice(string.ascii_letters + string.digits) for i in range(20)) # Generates a random address with 20 alphanumeric characters
+    mydict = {"name": name, "address": address} # Creates a dictionary with the name and address
+    x = mycol.insert_one(mydict) # Inserts the document into the collection
+
+print(f"{num_inserts} documents were inserted into the collection.")
