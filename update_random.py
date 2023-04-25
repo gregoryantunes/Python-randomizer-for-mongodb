8ea4333b2188e2b62403d738f4ec34053e89f485
Gregory M. Antunes Pires <68088354+gregoryantunes@users.noreply.github.com>
2023-04-25 19:33:14 -0300

    Update update_random.py

--- update_random.py
+++ update_random.py
@@ -6,13 +6,13 @@ myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["mydatabase"]
 mycol = mydb["customers"]
 
-num_updates = random.randint(30, 100) # Randomly chooses the number of updates to be performed
+num_updates = random.randint(30, 100) 
 
 for i in range(num_updates):
-    name = ''.join(random.choice(string.ascii_letters) for i in range(10)) # Generates a random name with 10 letters
-    address = ''.join(random.choice(string.ascii_letters + string.digits) for i in range(20)) # Generates a random address with 20 alphanumeric characters
-    filter = {"name": name} # Creates a filter to find a document with the randomly generated name
-    update = {"$set": {"address": address}} # Creates an update to change the address of the found document
-    mycol.update_one(filter, update) # Updates the document in the collection
+    name = ''.join(random.choice(string.ascii_letters) for i in range(10))
+    address = ''.join(random.choice(string.ascii_letters + string.digits) for i in range(20))
+    filter = {"name": name}
+    update = {"$set": {"address": address}}
+    mycol.update_one(filter, update)
 
 print(f"{num_updates} documents were updated in the collection.")
